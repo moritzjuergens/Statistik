1e53bb89fc8d650c021dadf7ae53f056072dc044
moritzjuergens <moritzjuergens@outlook.de>
2021-12-19 22:07:04 +0100

    integrated csv; started df selection; started output generation

--- app.R
+++ app.R
@@ -9,41 +9,40 @@
 
 library(shiny)
 
+covidData = read.csv("owid-covid-data.csv")
+populations = unique(covidData[c("location", "population")])
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Covid Data"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            selectInput("select", label = h3("Select box"), 
+                        choices = populations["location"], 
+                        selected = 1),
+            
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+          h2("Location"),
+           fluidRow(column(10, verbatimTextOutput("value"))),
+           h2("Population"),
+           fluidRow(column(10, verbatimTextOutput("value2")))
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+    
+  output$value <- renderPrint({ input$select })
+  output$value2 <- renderPrint({ populations[populations$location ==input$select, 2]})
+    
 }
 
 # Run the application 
